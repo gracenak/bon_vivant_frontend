9dd7fb312f2bda72acd55fdf4f40976007578708
gracenak <gracenak@gmail.com>
2021-04-22 11:55:24 -0700

    install composeWithDevTools, set up store in index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,25 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import manageRecipe from './reducers/manageRecipe'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 
-let store = createStore(manageRecipe, applyMiddleware(thunk))
+const composeEnhancers = composeWithDevTools({
+  // Specify here name, actionsBlacklist, actionsCreators and other options
+});
+ 
+// let store = createStore(manageRecipe, applyMiddleware(thunk))
+const middleware = [
+  thunk,
+];
+
+const store = createStore(
+  manageRecipe,
+  composeEnhancers(
+    applyMiddleware(...middleware)
+    // other store enhancers if any
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
