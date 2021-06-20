0bc65b2f13eb52dc7f71531f3329931ca174e3ce
gracenak <gracenak@gmail.com>
2021-06-19 18:53:20 -0700

    install npm package for star ratings and begin to implement star reatings

--- src/components/ReviewInput.js
+++ src/components/ReviewInput.js
@@ -2,6 +2,7 @@ import { Component } from 'react'
 import { connect } from 'react-redux'
 import { addReview } from '../actions/addReview'
 import styled from "styled-components";
+// import StarRatings from 'react-star-ratings'
 
 class ReviewInput extends Component {
 
@@ -29,6 +30,8 @@ class ReviewInput extends Component {
         })
     }
 
+
+
     render() {
         // const ratingOptions=[5,4,3,2,1].map((score, index) => {
         //     <input type="radio" name="rating" value={score} onChange={() => console.log('selected:', score)} id={`rating-${score}`}/>
@@ -51,16 +54,33 @@ class ReviewInput extends Component {
                     </select>
                     </Field>
                     <Field>
-                        <input type="radio" name="rating" value={this.state.rating} onChange={this.handleOnChange}/>
+                        {/* <input type="radio" name="rating" value={this.state.rating} onChange={this.handleOnChange}/> */}
                         <Fieldset>
-                        <span class="star-cb-group">
-                            <input type="radio" id="rating-5" name="rating" value="5" /><Label for="rating-5">5</Label>
-                            <input type="radio" id="rating-4" name="rating" value="4" checked="checked" /><label for="rating-4">4</label>
-                            <input type="radio" id="rating-3" name="rating" value="3" /><Label for="rating-3">3</Label>
-                            <input type="radio" id="rating-2" name="rating" value="2" /><Label for="rating-2">2</Label>
-                            <input type="radio" id="rating-1" name="rating" value="1" /><Label for="rating-1">1</Label>
-                            <input type="radio" id="rating-0" name="rating" value="0" class="star-cb-clear" /><Label for="rating-0">0</Label>
-                        </span>
+                        {/* <StarRatings
+          rating={this.state.rating}
+          starRatedColor="blue"
+          onChange={this.handleOnChange}
+          numberOfStars={5}
+          name='rating'
+        /> */}
+                        <RatingBox>
+                        <label class="radio-inline">
+                            <input type="radio" id="rating-5" name="rating" value="5" />
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" id="rating-4" name="rating" value="4" checked="checked" />
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" id="rating-3" name="rating" value="3" />
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" id="rating-2" name="rating" value="2" />
+                        </label>
+                         <label class="radio-inline">
+                            <input type="radio" id="rating-1" name="rating" value="1" />
+                        </label>
+                        {/* </span> */}
+                        </RatingBox>
                         </Fieldset>
                     </Field>
                 </RatingContainer>
@@ -137,6 +157,34 @@ const Fieldset = styled.div `
     margin: 1em auto;
      
 `
-const Label = styled.div `
-    display: inline-block
+
+
+const RatingBox = styled.div`
+  background: #fff;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  overflow: hidden;
+  flex-direction: row-reverse;
+  position: relative;
+  input { display: none; }
+  label {
+    cursor: pointer;
+    width: 40px;
+    height: 40px;
+    margin-top: auto;
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='126.729' height='126.73'%3e%3cpath fill='%23e3e3e3' d='M121.215 44.212l-34.899-3.3c-2.2-.2-4.101-1.6-5-3.7l-12.5-30.3c-2-5-9.101-5-11.101 0l-12.4 30.3c-.8 2.1-2.8 3.5-5 3.7l-34.9 3.3c-5.2.5-7.3 7-3.4 10.5l26.3 23.1c1.7 1.5 2.4 3.7 1.9 5.9l-7.9 32.399c-1.2 5.101 4.3 9.3 8.9 6.601l29.1-17.101c1.9-1.1 4.2-1.1 6.1 0l29.101 17.101c4.6 2.699 10.1-1.4 8.899-6.601l-7.8-32.399c-.5-2.2.2-4.4 1.9-5.9l26.3-23.1c3.8-3.5 1.6-10-3.6-10.5z'/%3e%3c/svg%3e");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 76%;
+    transition: .3s;
+  }
+  input:checked ~ label, input:checked ~ label ~ label {
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='126.729' height='126.73'%3e%3cpath fill='%23fcd93a' d='M121.215 44.212l-34.899-3.3c-2.2-.2-4.101-1.6-5-3.7l-12.5-30.3c-2-5-9.101-5-11.101 0l-12.4 30.3c-.8 2.1-2.8 3.5-5 3.7l-34.9 3.3c-5.2.5-7.3 7-3.4 10.5l26.3 23.1c1.7 1.5 2.4 3.7 1.9 5.9l-7.9 32.399c-1.2 5.101 4.3 9.3 8.9 6.601l29.1-17.101c1.9-1.1 4.2-1.1 6.1 0l29.101 17.101c4.6 2.699 10.1-1.4 8.899-6.601l-7.8-32.399c-.5-2.2.2-4.4 1.9-5.9l26.3-23.1c3.8-3.5 1.6-10-3.6-10.5z'/%3e%3c/svg%3e");
+  }
+  input:not(:checked) ~ label:hover,
+  input:not(:checked) ~ label:hover ~ label {
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='126.729' height='126.73'%3e%3cpath fill='%23d8b11e' d='M121.215 44.212l-34.899-3.3c-2.2-.2-4.101-1.6-5-3.7l-12.5-30.3c-2-5-9.101-5-11.101 0l-12.4 30.3c-.8 2.1-2.8 3.5-5 3.7l-34.9 3.3c-5.2.5-7.3 7-3.4 10.5l26.3 23.1c1.7 1.5 2.4 3.7 1.9 5.9l-7.9 32.399c-1.2 5.101 4.3 9.3 8.9 6.601l29.1-17.101c1.9-1.1 4.2-1.1 6.1 0l29.101 17.101c4.6 2.699 10.1-1.4 8.899-6.601l-7.8-32.399c-.5-2.2.2-4.4 1.9-5.9l26.3-23.1c3.8-3.5 1.6-10-3.6-10.5z'/%3e%3c/svg%3e");
+  }
 `
+
